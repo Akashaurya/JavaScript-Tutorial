3918ee763afe0bc83d09384c4c0e672d4f40d900
Akashaurya <139988593+Akashaurya@users.noreply.github.com>
2023-09-03 15:56:52 +0000

    study the difference between var ,let and const

---
+++ chapter-1/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1215
+var accoundEmail = "akashjai0009@gmail.com"
+let accountPassord = "1245585"
+ accountCity = " jaipur"
+  let accountState;
+//accountId = 415 const can't be change
+ accoundEmail = "hsdj@jhds"
+ accountPassord = "555"
+ accountCity = "Lucknow"
+ /*
+ Perfer not to use var
+ use let beccause issue in functionl cade and scope code
+ */
+console.table([accountId , accoundEmail , accountCity, accountPassord, accountState]);
