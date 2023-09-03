4cd4e6135c848ec1b07ce2447752b59374604c22
Akashaurya <139988593+Akashaurya@users.noreply.github.com>
2023-09-03 16:26:16 +0000

    DataTypes

---
+++ chapter-1/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // test all your JS code as new version
+
+//alert (3+5) // we are using nodejs not browser
+ console.log (3+8)
+ console.log( "Akash");
+
+ let name = "akash"
+ let age = 20
+ let islogin = false
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined
+ //symbol => unique
+
+ // object
