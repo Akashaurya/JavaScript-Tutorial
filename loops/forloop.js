37673051613a82f8145bda50c7abf83e040fcb6a
Akashaurya <139988593+Akashaurya@users.noreply.github.com>
2023-09-12 06:50:10 +0000

    loops commend

---
+++ loops/forloop.js
@@ -0,0 +1,31 @@
+//for (let index = 0; index < 10; index++) {
+  //  const element =index;
+    //console.log(index);
+//}
+ let myArray = ["aksah","RJ Rohit","Rfu"]
+ for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+ }
+
+ //break and continue
+//for (let i= 0; i <=20; i++) {
+  //if (i==5) {
+    //console.log(`detecte five`);
+   // break
+    
+  //}
+  //const element = ("i");
+  //console.log(`the elements in ${i}`);
+  
+//}
+for (let i= 0; i <=20; i++) {
+  if (i==5) {
+    console.log(`detecte five`);
+    continue
+    
+  }
+  const element = ("i");
+  console.log(`the elements in ${i}`);
+  
+}
